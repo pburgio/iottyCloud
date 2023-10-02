31e0d1501599aaedcdd2a8c6f20edc37f636c6be
Paolo Burgio <paolo.burgio@gmail.com>
2023-10-02 15:14:05 +0200

    Moved test folder inside package folder

--- src/iottycloud/tests/helpers.py
+++ src/iottycloud/tests/helpers.py
@@ -4,6 +4,6 @@ from iottycloud.cloudapi import CloudApi
 
 class CloudApiConcrete(CloudApi):
     """Concrete class to enable testing"""
-    
+
     async def async_get_access_token(self) -> str:
         return "some-token"
--- src/iottycloud/tests/test_create.py
+++ src/iottycloud/tests/test_create.py
@@ -1,21 +1,24 @@
-"""Test for creation of CloudApi proxy"""
+"""Test for creation of CloudApi proxy."""
 import requests
 
 from .helpers import CloudApiConcrete
 
 def test_creation_noparams_ws_error():
+    """No 'WS' param causing errors."""
     try:
         _ = CloudApiConcrete(None, "host", "client_id")
     except ValueError as excinfo:
         assert str(excinfo) == "websession"
-              
+
 def test_creation_noparams_host_error():
+    """No 'host' param causing errors."""
     try:
         _ = CloudApiConcrete(requests.Session(), None, "client_id")
     except ValueError as excinfo:
         assert str(excinfo) == "host"
 
 def test_creation_noparams_clientid_error():
+    """No 'clientid' param causing errors."""
     try:
         _ = CloudApiConcrete(requests.Session(), "host", None)
     except ValueError as excinfo:
